6d6c9d9f280d4fbdaca83c895296151c4ad3ae09
jiagengliu <3200105174@zju.edu.cn>
2024-10-29 17:56:31 -0400

    1

--- convert.py
+++ convert.py
@@ -5,21 +5,14 @@ def flac_to_wav_and_save(source_folder):
     for dirpath, dirnames, filenames in os.walk(source_folder):
             for filename in filenames:
                 if filename.endswith('.flac'):
-                    try:
-                        # 构建完整的 FLAC 文件路径
-                        flac_path = os.path.join(dirpath, filename)
-                        print("FLAC Path:", flac_path)  # 打印查看FLAC文件路径
-
-                        # 构建 WAV 文件路径
-                        wav_path = os.path.join(dirpath, os.path.splitext(filename)[0] + '.wav')
-
-                        # 读取并转换音频文件
-                        audio = AudioSegment.from_file(flac_path, "flac")
-                        audio.export(wav_path, format="wav")
-
-                        print(f"Converted {filename} to WAV format.")
-                    except Exception as e:
-                        print(f"Failed to convert {filename}: {str(e)}")
+                    flac_path = os.path.join(dirpath, filename)
+                    print("FLAC Path:", flac_path)  # 打印查看FLAC文件路径
+                    wav_path = os.path.join(dirpath, os.path.splitext(filename)[0] + '.wav')
+                    audio = AudioSegment.from_file(flac_path, "flac")
+                    # audio.export(wav_path, format="wav")
+                    if os.path.exists(wav_path):
+                            os.remove(flac_path)
+                    print(f"remove:{flac_path}")
 
 # 调用函数
 source_folder = './data/LibriSpeech'
